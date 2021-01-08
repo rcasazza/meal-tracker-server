bf46ff3022b02d3803e09ff075ae4fec789a089e
Bob Casazza <rcasazza@gmail.com>
2021-01-07 19:26:03 -0500

    complete meals flow

--- src/routes/getMealsRoute.js
+++ src/routes/getMealsRoute.js
@@ -1,11 +1,11 @@
-import { getMeals } from '../db';
+import { getPopulatedMeals } from '../db';
 
 export const getMealsRoute = {
     method: 'get',
     path: '/meals',
     handler: async (req, res) => {
 //        console.log("Getting meals");
-        const meals = await getMeals();
+        const meals = await getPopulatedMeals();
         res.status(200).json(meals);
     },
 };
