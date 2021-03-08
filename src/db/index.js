fc1d7b09c04e5c294246a02a914ee32eabb74f6c
Bob Casazza <rcasazza@gmail.com>
2021-03-08 07:59:39 -0500

    add delete ingredient, delete meal, add ingredient

--- src/db/index.js
+++ src/db/index.js
@@ -1,2 +1,7 @@
 export { db } from './db';
 export { getPopulatedMeals } from './getPopulatedMeals';
+export { getIngredients } from './getIngredients';
+export { deleteMeal } from './deleteMeal';
+export { deleteIngredient } from './deleteIngredient';
+export { insertIngredient } from './insertIngredient';
+
--- src/routes/addIngredientRoute.js
+++ src/routes/addIngredientRoute.js
@@ -1,7 +1,13 @@
+import { getIngredients } from "../db";
+import { insertIngredient } from "../db";
+
 export const addIngredientRoute = {
     method: 'post',
     path: '/ingredients',
     handler: async (req, res) => {
-
+        const ingredient = req.body;
+        await insertIngredient(ingredient);
+        const updatedIngredients = await getIngredients();
+        res.status(200).json(updatedIngredients);
     },
 };
